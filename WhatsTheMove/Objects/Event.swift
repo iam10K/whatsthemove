23ae4df5dbbeb28ffef5bf3c09745fd0cb463ae7
Tyler Encke <tylerencke@gmail.com>
2016-11-15 02:11:32 -0500

    New Event is coming along, down to the last parts making it work smoothly and adding it to the database. (Some of the code I lost when rebasing)

--- WhatsTheMove/Objects/Event.swift
+++ WhatsTheMove/Objects/Event.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2016 WhatsTheMove. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class Event: NSObject {
+    
+    var eventDescription: String = ""
+    var endDate: Date = Date()
+    var ended: String = ""
+    var entryNote: String = ""
+    var address: String = ""
+    var latitude: Double = 0.0
+    var longitude: Double = 0.0
+    var name: String = ""
+    var privacyLevel: Int = 0
+    var sponsor: String = ""
+    var startDate: Date = Date()
+    var title: String = ""
+}
--- WhatsTheMove/Views/SelectDateViewController.swift
+++ WhatsTheMove/Views/SelectDateViewController.swift
@@ -11,7 +11,11 @@ import FSCalendar
 
 class SelectDateViewController: UIViewController, FSCalendarDataSource, FSCalendarDelegate {
 
+    var newEvent: Event?
+    var selectingStartDate: Bool = true
+    
     @IBOutlet weak var calendar: FSCalendar!
+    @IBOutlet weak var timePicker: UIDatePicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,21 +23,23 @@ class SelectDateViewController: UIViewController, FSCalendarDataSource, FSCalend
         // Do any additional setup after loading the view.
         calendar.placeholderType = FSCalendarPlaceholderType.none
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if selectingStartDate {
+            title = "Start Date"
+        } else {
+            title = "End Date"
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func doneSelectingDate(_ sender: AnyObject) {
+        
     }
-    */
+    
 
 }
